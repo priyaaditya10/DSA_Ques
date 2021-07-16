1451be390bf82c8def7d84b677a8729e5314cf98
priyaaditya10 <priyaaditya2207@gmail.com>
2021-07-16 09:58:09 +0530

    updated array questions

---
+++ arrays/smallest_subrraysum_greaterthan_givensum.cpp
@@ -0,0 +1,63 @@
+/*Given an array of integers (A[])  and a number x, find the smallest subarray with sum greater than the given value.
+
+Note: The answer always exists. It is guaranteed that x doesn't exceed the summation of a[i] (from 1 to N). */
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+class Solution
+{
+public:
+    int sb(int arr[], int n, int x)
+    {
+        // Initialize current sum and minimum length
+        int curr_sum = 0, min_len = n + 1;
+
+        // Initialize starting and ending indexes
+        int start = 0, end = 0;
+        while (end < n)
+        {
+            // Keep adding array elements while current sum
+            // is smaller than x
+            while (curr_sum <= x && end < n)
+                curr_sum += arr[end++];
+
+            // If current sum becomes greater than x.
+            while (curr_sum > x && start < n)
+            {
+                // Update minimum length if needed
+                if (end - start < min_len)
+                    min_len = end - start;
+                // keep on subtracting the starting element to get the subarray of min length
+                curr_sum -= arr[start++];
+            }
+        }
+       // to check if sum aof all array elements is less than given sum
+        if (min_len > n)
+            return 0;
+
+        return min_len;
+    }
+};
+
+
+
+int main()
+{
+    
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, x;
+        cin >> n >> x;
+        int a[n];
+        for (int i = 0; i < n; i++)
+            cin >> a[i];
+        Solution obj;
+        cout << obj.sb(a, n, x) << endl;
+    }
+    return 0;
+} 
